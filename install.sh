d85f9dc6ffa5655a43d23a59f880e5e7bdbfd4c1
marcellmueller <mlmueller@protonmail.com>
2020-10-08 14:21:42 -0700

    changes to install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get dist-upgrade -y
 sudo apt install -y i3 i3blocks rofi redshift-gtk 
 sudo apt install -y steam autokey-gtk nautilus git curl htop tlp powertop gimp libreoffice 
+sudo apt install -y nodejs build-essential 
 sudo apt install -y vagrant virtualbox virtualbox-guest-utils virtualbox-guest-dkms linux-headers-generic 
 sudo snap install chromium
 sudo snap remove software-boutique
 sudo snap remove ubuntu-mate-welcome
 sudo apt remove -y caja apport bluez pluma
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install -y apt-transport-https
+sudo apt-get update -y
+sudo apt-get install -y code
 dconf write /org/mate/desktop/session/required-components/windowmanager "'i3'"
 dconf write /org/mate/desktop/background/show-desktop-icons "false"
 mv i3 i3status redshift rofi ~/.config
