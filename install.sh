9ceb8743bf63d1f410d9e4d5698ee430e26d78f6
marcellmueller <mlmueller@protonmail.com>
2021-02-04 14:02:15 -0800

    Removed lighthouse/vagrant stuff

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt install -y vagrant virtualbox virtualbox-guest-utils virtualbox-guest-d
 sudo snap install chromium
 sudo snap remove software-boutique
 sudo snap remove ubuntu-mate-welcome
-sudo apt remove -y caja apport bluez pluma
+sudo apt remove -y caja apport pluma
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -21,11 +21,6 @@ sudo apt-get update -y
 sudo apt-get install -y code
 dconf write /org/mate/desktop/session/required-components/windowmanager "'i3'"
 dconf write /org/mate/desktop/background/show-desktop-icons "false"
-cd
-mkdir lighthouse
-cd lighthouse
-curl -O http://d10ofk0qhbh8u9.cloudfront.net/vagrant/Vagrantfile
-vagrant up
 git config --global user.name "marcellmueller"
 git config --global user.email "mlmueller@protonmail.com"
 git config --global color.ui auto
