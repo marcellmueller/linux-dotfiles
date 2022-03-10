74da01e3afef78dcb49182e13d72f23723830ad4
marcellmueller <mlmueller@protonmail.com>
2022-03-10 08:00:49 -0800

    Add Gcloud CLI and Docker to install script

--- install.sh
+++ install.sh
@@ -12,19 +12,29 @@ sudo snap install chromium
 sudo snap remove software-boutique
 sudo snap remove ubuntu-mate-welcome
 sudo apt remove -y caja apport pluma
+sudo apt install apt-transport-https ca-certificates gnupg lsb-release
 
 # VSCode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install -y apt-transport-https
 
 # Github CLI
 url -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
+# Gcloud CLI
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
 sudo apt update -y
-sudo apt install -y code gh
+sudo apt install -y code gh google-cloud-cli
+sudo apt install docker-ce docker-ce-cli containerd.io
 
 # Replace Mate window manager with i3
 dconf write /org/mate/desktop/session/required-components/windowmanager "'i3'"
