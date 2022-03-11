358377d93bac0f2ea6a832ef13d4b44d50535f14
marcellmueller <mlmueller@protonmail.com>
2022-03-11 08:31:51 -0800

    Add kubectl repository to install script

--- install.sh
+++ install.sh
@@ -31,10 +31,13 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Kubectl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update -y
-sudo apt install -y code gh google-cloud-cli
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y code gh google-cloud-cli kubectl
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Replace Mate window manager with i3
 dconf write /org/mate/desktop/session/required-components/windowmanager "'i3'"
